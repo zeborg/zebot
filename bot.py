8838e0f7dc31892f4b6fa2e4e3c0e94b8beed1fc
zeborg <37282098+zeborg@users.noreply.github.com>
2019-04-17 13:41:02 +0530

    Fixed code repetition, added embed footer

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 '''
 
-zebot v0.2b [Discord BOT by zeborg]
+zebot v0.2c [Discord BOT by zeborg]
 
 GitHub/zeborg  |  Discord: zeborg#4589
 
@@ -23,24 +23,36 @@ client = discord.Client()
 server_start_date = datetime.now().strftime("%x")
 server_start_time = datetime.now().strftime("%X")
 
+verinfo = 'zebot v0.2c' # ZEBOT CURRENT VERSION
 b_t = 'zeb' # ZEBOT COMMAND TRIGGER
+
+
+@client.event
+async def on_ready():
+    ''' CHANGE CLIENT PRESENCE AT BOT START UP, PRINT LIST OF SERVERS CONNECTED DURING START UP IN BOT CONSOLE '''
+
+    await client.change_presence(activity=discord.Game(name = 'GitHub/zeborg | zeb help'))
+    print('\nConnected to ' + str(len(client.guilds)) + f' servers after recent reboot at {datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC.')
+    for i in range(len(client.guilds)): print(f'{i+1}. {client.guilds[i]} | ID: {client.guilds[i].id} | Owner: {client.guilds[i].owner} | Members: {len(client.guilds[i].members)}')
+    print('')
 
 
 @client.event
 async def on_guild_join(guild):
     ''' LOGGING GUILD CONNECTION '''
 
-    logging_server = discord.utils.get(client.guilds, name = 'zebot dev')
+    logging_server = discord.utils.get(client.guilds, name='zebot dev')
     channel_log_console = discord.utils.get(logging_server.text_channels, name = 'console-logs')
 
     await channel_log_console.send(f'**CONNECTED** : ` {guild.name} ` | **ID** : ` {guild.id} ` | **OWNER** : ` {guild.owner} ` | **MEMBERS** : ` {len(guild.members)} `')
     print(f'CONNECTED : {guild.name} | ID : {guild.id} | OWNER : {guild.owner}') # LOGGING TO BOT SYSTEM CONSOLE
 
+
 @client.event
 async def on_guild_remove(guild):
     ''' LOGGING GUILD DISCONNECTION '''
 
-    logging_server = discord.utils.get(client.guilds, name = 'zebot dev')
+    logging_server = discord.utils.get(client.guilds, name='zebot dev')
     channel_log_console = discord.utils.get(logging_server.text_channels, name = 'console-logs')
 
     await channel_log_console.send(f'**DISCONNECTED** : ` {guild.name} ` | **ID** : ` {guild.id} ` | **OWNER** : ` {guild.owner} | **MEMBERS** : {len(guild.members)} `')
@@ -48,40 +60,14 @@ async def on_guild_remove(guild):
 
 
 @client.event
-async def on_ready():
-    ''' CHANGE CLIENT PRESENCE AT BOT START UP, PRINT LIST OF SERVERS CONNECTED DURING START UP IN BOT CONSOLE '''
-
-    await client.change_presence(activity=discord.Game(name = "GitHub/zeborg | zeb help"))
-    print('\nConnected to ' + str(len(client.guilds)) + f' servers after recent reboot at {datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC.')
-    for i in range(len(client.guilds)): print(f'{i+1}. {client.guilds[i]} | ID: {client.guilds[i].id} | Owner: {client.guilds[i].owner} | Members: {len(client.guilds[i].members)}')
-    print('')
-
-@client.event
-async def on_guild_join(guild):
-    ''' LOGGING GUILD CONNECTION '''
-
-    logging_server = discord.utils.get(client.guilds, name = 'zebot dev')
-    channel_log_console = discord.utils.get(logging_server.text_channels, name = 'console-logs')
-
-    await channel_log_console.send(f'**CONNECTED** : ` {guild.name} ` | **ID** : ` {guild.id} ` | **OWNER** : ` {guild.owner} ` | **MEMBERS** : ` {len(guild.members)} `')
-    print(f'CONNECTED : {guild.name} | ID : {guild.id} | OWNER : {guild.owner}')  # LOGGING TO BOT SYSTEM CONSOLE
-
-
-@client.event
-async def on_guild_remove(guild):
-    ''' LOGGING GUILD DISCONNECTION '''
-
-    logging_server = discord.utils.get(client.guilds, name = 'zebot dev')
-    channel_log_console = discord.utils.get(logging_server.text_channels, name = 'console-logs')
-
-    await channel_log_console.send(f'**DISCONNECTED** : ` {guild.name} ` | **ID** : ` {guild.id} ` | **OWNER** : ` {guild.owner} | **MEMBERS** : {len(guild.members)} `')
-    print(f'DISCONNECTED : {guild.name} | ID : {guild.id} | OWNER : {guild.owner} | MEMBERS : {len(guild.members)}')  # LOGGING TO BOT SYSTEM CONSOLE
-
-@client.event
 async def on_message(message):
     ''' GUILD TEXT MESSAGE RESPONSES '''
 
+    # DECLARING LOGGING SERVER AND CHANNELS
     logging_server = discord.utils.get(client.guilds, name='zebot dev')
+    channel_log_server = discord.utils.get(logging_server.text_channels, name='server-logs')
+    channel_log_server_bots = discord.utils.get(logging_server.text_channels, name='server-bots-logs')
+    channel_log_zebdm = discord.utils.get(logging_server.text_channels, name='zebot-dm-logs')
 
     def random_colors():
         ''' RANDOM STOCK COLOUR GENERATOR '''
@@ -154,17 +140,14 @@ async def on_message(message):
     display_message_content = f'```{message.content}```' if len(message.content) > 0 else '`No content`'
 
     # LOGGING TEXTS FROM CONNECTED GUILDS
-    channel_log_server = discord.utils.get(logging_server.text_channels, name = 'server-logs')
-    channel_log_server_bots = discord.utils.get(logging_server.text_channels, name = 'server-bots-logs')
-    channel_log_zebdm = discord.utils.get(logging_server.text_channels, name='zebot-dm-logs')
-
     if message.type == discord.MessageType.default:
         if message.guild != None: # LOGGING TEXTS RECEIVED IN GUILD CHANNELS
             if not message.author.bot: await channel_log_server.send((f'**{message.author}** logged text (*{datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC*) in **{message.author.guild}** `#{message.channel}`:\n\nAttachment: `{get_attached_file_url(message)}`\n\nContent: {display_message_content}'))
             elif message.author.bot and message.author != client.user: await channel_log_server_bots.send((f'**{message.author}** logged text (*{datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC*) in **{message.author.guild}** `#{message.channel}`:\n\nAttachment: `{get_attached_file_url(message)}`\n\nContent: {display_message_content}'))
         elif message.guild == None: # LOGGING TEXTS RECEIVED IN ZEBOT DM
-            if not message.author.bot: await channel_log_zebdm.send((f'**{message.author}** logged text (*{datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC*) in **{message.channel}**:\n\nAttachment: `{get_attached_file_url(message)}`\n\nContent: {display_message_content}'))
-            elif message.author.bot and message.author != client.user: await channel_log_zebdm.send((f'**{message.author}** *__[BOT]__* logged text (*{datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC*) in **{message.channel}**:\n\nAttachment: `{get_attached_file_url(message)}`\n\nContent: {display_message_content}'))
+            if not message.author.bot: await channel_log_zebdm.send((f'**{message.author}** logged text (*{datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC*) in `{message.channel}`:\n\nAttachment: `{get_attached_file_url(message)}`\n\nContent: {display_message_content}'))
+            elif message.author.bot and message.author != client.user: await channel_log_zebdm.send((f'**{message.author}** *__[BOT]__* logged text (*{datetime.now().strftime("%x")} {datetime.now().strftime("%X")} UTC*) in `{message.channel}``:\n\nAttachment: `{get_attached_file_url(message)}`\n\nContent: {display_message_content}'))
+
     # GREET A USER
     if message.content.startswith(f'{b_t} greet') and '@everyone' in message.content: await message.channel.send(f':bouquet: Hey **everyone**! Greetings from {message.author.mention} :bouquet:')
     elif message.content.startswith(f'{b_t} greet') and ((len(message.mentions) > 1 and '@everyone' not in message.content)): await message.channel.send(f':bouquet: Hey {multi_mentions()}! Greetings from {message.author.mention}! :bouquet:')
@@ -191,18 +174,23 @@ async def on_message(message):
         await message.channel.send(f'**__SERVER ROLES__**:\n\n{role_str}')
 
     # SHOW AVATAR
-    if message.content.lower() == f'{b_t} avatar':
-        ava_embed = discord.Embed(title = f'{message.author.name}\'s Avatar', url = f'{message.author.avatar_url}', colour = random_colors())
-        ava_embed.set_image(url = f'{message.author.avatar_url}')
-        await message.channel.send(embed = ava_embed)
-    elif message.content.startswith(f'{b_t} avatar'):
-        if len(message.mentions) > 0:
-            ava_embed = discord.Embed(title = f'{message.mentions[0].name}\'s Avatar', url = f'{message.mentions[0].avatar_url}', colour = random_colors())
-            ava_embed.set_image(url = f'{message.mentions[0].avatar_url}')
+    if message.content.startswith(f'{b_t} avatar'):
+        passed = False
+        if len(message.mentions) == 1:
+            passed = True
+            requested_uinfo = message.mentions[0]
+        elif message.content.lower() == f'{b_t} avatar':
+            passed = True
+            requested_uinfo = message.author
+
+        if passed:
+            ava_embed = discord.Embed(title = f'{requested_uinfo.name}\'s Avatar', url = f'{requested_uinfo.avatar_url}', colour = random_colors())
+            ava_embed.set_image(url = f'{requested_uinfo.avatar_url}')
+            ava_embed.set_footer(text = f'{verinfo}', icon_url = f'{client.user.avatar_url}')
             await message.channel.send(embed = ava_embed)
         else: await message.channel.send('To show someone\'s avatar by mentioning them, type `zeb avatar <@User>`\nTo show your own avatar, type `zeb avatar`')
 
-    # TEXT CHANNELS IN THE GUILD
+    # VIEW USER INFO
     if message.guild != None:
         passed = False
         if message.content.startswith(f'{b_t} userinfo'):
@@ -221,10 +209,12 @@ async def on_message(message):
                 user_embed.add_field(name = 'User ID', value = f'{requested_uinfo.id}')
                 user_embed.add_field(name = 'Current Status', value = f'{str(requested_uinfo.status).upper()}')
                 user_embed.add_field(name = 'Server Nickname', value = f'{requested_uinfo.nick}')
-                user_embed.add_field(name = 'Highest Role', value = f'{requested_uinfo.top_role}')
+                user_embed.add_field(name = 'Highest Role', value = 'OWNER' if requested_uinfo.id == message.guild.owner_id else f'{requested_uinfo.top_role}')
                 user_embed.add_field(name = 'Joined Discord', value = f'{requested_uinfo.created_at}')
                 user_embed.add_field(name = 'Joined Server', value = f'{requested_uinfo.joined_at}')
+                user_embed.set_footer(text = f'{verinfo}', icon_url = f'{client.user.avatar_url}')
                 await message.channel.send(embed = user_embed)
             else: await message.channel.send(f'To view someone\'s userinfo by mentioning them, type `{b_t} userinfo <@user>`\nTo view your own userinfo, type `{b_t} userinfo`')
+
 
 client.run(token)
